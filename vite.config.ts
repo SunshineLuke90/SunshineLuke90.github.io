c5e5633a5172376bdfaa72d25df1dcd21167fc12
Lucius Creamer <sunshineluke90@gmail.com>
2025-08-06 23:40:02 -0500

    Messing around with build settings. Didn't get anywhere.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        // 'main' and 'nested' are arbitrary names for your entry points
+        // 'index.html' is still included here for demonstration, you can replace it
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'), // Your custom HTML entry
+        contact: resolve(__dirname, 'contact.html'), // Another custom HTML entry
+        project1: resolve(__dirname, 'project1.html')
+      }
+    }
+  }
 })
