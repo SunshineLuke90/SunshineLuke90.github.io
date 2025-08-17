ab992e6fa9aaa8445dd17cc55c1378e10bea5c3b
Lucius Creamer <sunshineluke90@gmail.com>
2025-08-17 17:33:11 -0500

    Implemented graph and on hover, with adjusted fields. Will need to update the graph so that it is larger, and potentially add more charts.

--- onHover/main.jsx
+++ onHover/main.jsx
@@ -1,12 +1,14 @@
 import '@esri/calcite-components/dist/components/calcite-button';
 import '@esri/calcite-components/dist/components/calcite-action-bar';
 import '@arcgis/map-components/components/arcgis-map';
-import { Chart } from 'chart.js';
+import { Chart, CategoryScale, LinearScale, BarController, BarElement } from 'chart.js';
 import { disableZooming } from "./components/disableZoom";
 
 const mapElement = document.querySelector("arcgis-map");
 const view = mapElement.view;
 disableZooming(view);
+Chart.register(BarController, BarElement, CategoryScale, LinearScale)
+
 
 view.popup.dockOptions = {
     // Disable the dock button so users cannot undock the popup
@@ -20,13 +22,56 @@ view.popup.dockOptions = {
 };
 
 // fields used to display demographics info
+/*
 const demographicFields = {
     NAME: "Name",
     P0120002: "Male Population",
     P0120026: "Female Population",
 };
+*/
+const femaleFields = ["P0120027", "P0120028", "P0120029", "F1519", "F2025", "P0120035", "P0120036", "P0120037", "P0120038", "P0120039", "P0120040", "P0120041", "F6064", "F6569", "P0120046", "P0120047", "P0120048", "P0120049"]
+const maleFields = ["P0120003", "P0120004", "P0120005", "M1519", "M2024", "P0120011", "P0120012", "P0120013", "P0120014", "P0120015", "P0120016", "P0120017", "M6064", "M6569", "P0120022", "P0120023", "P0120024", "P0120025"]
+const demographicFields = {
+    NAME: "Name",
+    P0120003: "Male 0-4",
+    P0120004: "Male 5-9",
+    P0120005: "Male 10-14",
+    M1519: "Male 15-19",
+    M2024: "Male 20-24",
+    P0120011: "Male 25-29",
+    P0120012: "Male 30-34",
+    P0120013: "Male 35-39",
+    P0120014: "Male 40-44",
+    P0120015: "Male 45-49",
+    P0120016: "Male 50-54",
+    P0120017: "Male 55-59",
+    M6064: "Male 60-64",
+    M6569: "Male 65-69",
+    P0120022: "Male 70-74",
+    P0120023: "Male 75-79",
+    P0120024: "Male 80-84",
+    P0120025: "Male 85+",
+    P0120027: "Female 0-4",
+    P0120028: "Female 5-9",
+    P0120029: "Female 10-14",
+    F1519: "Female 15-19",
+    F2025: "Female 20-24",
+    P0120035: "Female 25-29",
+    P0120036: "Female 30-34",
+    P0120037: "Female 35-39",
+    P0120038: "Female 40-44",
+    P0120039: "Female 45-49",
+    P0120040: "Female 50-54",
+    P0120041: "Female 55-59",
+    F6064: "Female 60-64",
+    F6569: "Female 65-69",
+    P0120046: "Female 70-74",
+    P0120047: "Female 75-79",
+    P0120048: "Female 80-84",
+    P0120049: "Female 85+"
+}
 
-let highlight, currentId
+let highlight, currentId, chart
 
 view.highlights = [{
     name: "default",
@@ -71,49 +116,48 @@ view.when(() => {
             // the topmost graphic from the demographics layer and display attribute
             // values from the graphic to the user
             const hitGraphic = response.results[0].graphic;
-            const objectId = hitGraphic.attributes.OBJECTID;
+            const FID = hitGraphic.attributes.FID;
 
-            if (highlight && currentId === objectId) {
+            if (highlight && currentId === FID) {
                 return;
             }
 
             // highlight all features belonging to the same demographic as the feature
             // returned from the hitTest
             const query = layer.createQuery();
-            query.where = `OBJECTID = '${objectId}'`;
-            query.outFields = ["NAME", "P0120002", "P0120026", "OBJECTID"];
+            query.where = `FID = '${FID}'`;
+            query.outFields = ["NAME", "FID", "P0120049", "P0120048", "P0120047", "P0120046", "F6569", "F6064", "P0120041", "P0120040", "P0120039", "P0120038", "P0120037", "P0120036", "P0120035", "F2025", "F1519", "P0120029", "P0120028", "P0120027", "P0120025", "P0120024", "P0120023", "P0120022", "M6569", "M6064", "P0120017", "P0120016", "P0120015", "P0120014", "P0120013", "P0120012", "P0120011", "M2024", "M1519", "P0120005", "P0120004", "P0120003"];
             query.returnGeometry = false;
 
             const featureSet = await layer.queryFeatures(query);
 
             if (featureSet.features.length > 0) {
                 const graphic = featureSet.features[0];
-                currentId = graphic.attributes.OBJECTID;
+                currentId = graphic.attributes.FID;
                 highlight?.remove();
                 highlight = layerView.highlight(graphic);
-                document.getElementById("chart").innerHTML = setupChartDiv(graphic.attributes);
+                document.getElementById("chart").innerHTML = queryData(graphic.attributes);
+
             }
 
-            function setupChartDiv(attributes) {
+            function queryData(attributes) {
                 let isFirst = true;
-                /* 
+
                 //Preparation for using the chart for data display. 
                 // First need to optimize dataset to be able to provide data more cleanly.
-                console.log(attributes)
                 const femaleAgeData = []
                 const maleAgeData = []
 
                 for (let key in attributes) {
-                    if (key.includes("26")) {
+                    if (femaleFields.includes(key)) {
                         femaleAgeData.push(attributes[key])
                     }
-                    else if (key.includes("0002")) {
-                        maleAgeData.push(attributes[key])
+                    else if (maleFields.includes(key)) {
+                        maleAgeData.push(-Math.abs(attributes[key]))
                     }
                 }
-                console.log(femaleAgeData)
-                console.log(maleAgeData)
-                */
+                updateChart([femaleAgeData, maleAgeData])
+
                 const htmls = Object.keys(demographicFields)
                     .map((name) => {
                         if (attributes[name] != null) {
@@ -127,6 +171,85 @@ view.when(() => {
                 return htmls.join("");
             }
 
+
+            function updateChart(newData) {
+                const [femaleAgeData, maleAgeData] = newData;
+
+                if (!chart) {
+                    const canvasElement = document.getElementById("chart");
+                    chart = new Chart(canvasElement.getContext("2d"), {
+                        type: "bar",
+                        data: {
+                            labels: [
+                                "85+",
+                                "80-84",
+                                "75-79",
+                                "70-74",
+                                "65-69",
+                                "60-64",
+                                "55-59",
+                                "50-54",
+                                "45-49",
+                                "40-44",
+                                "35-39",
+                                "30-34",
+                                "25-29",
+                                "20-24",
+                                "15-19",
+                                "10-14",
+                                "5-9",
+                                "0-4",
+                            ],
+                            datasets: [
+                                {
+                                    label: "Female",
+                                    backgroundColor: "#B266FF",
+                                    borderColor: "#7F00FF",
+                                    borderWidth: 0.25,
+                                    data: femaleAgeData,
+                                },
+                                {
+                                    label: "Male",
+                                    backgroundColor: "#0080FF",
+                                    borderColor: "#004C99",
+                                    borderWidth: 0.25,
+                                    data: maleAgeData,
+                                },
+                            ],
+                        },
+                        options: {
+                            indexAxis: "y",
+                            responsive: false,
+                            legend: { position: "bottom" },
+                            title: { display: true, text: "Population pyramid" },
+                            scales: {
+                                x: {
+                                    barThickness: 10,
+                                    scaleLabel: { display: true, labelString: "Population" },
+                                },
+                                y: {
+                                    scaleLabel: { display: true, labelString: "Age Group" },
+                                    stacked: true,
+                                },
+                            },
+                            tooltips: {
+                                callbacks: {
+                                    label: (tooltipItem, data) => {
+                                        return `${data.datasets[tooltipItem.datasetIndex].label}: ${numberWithCommas(Math.abs(tooltipItem.xLabel))}`;
+                                    },
+                                },
+                            },
+                        },
+                    });
+                } else {
+                    chart.data.datasets[0].data = femaleAgeData;
+                    chart.data.datasets[1].data = maleAgeData;
+                    chart.update();
+                }
+            }
+            function numberWithCommas(value) {
+                return (value || 0).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            }
         }
     }
 })
